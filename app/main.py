ec734228342045891422080d2afe95726ad7423d
r4m4 <ramahany317@gmail.com>
2026-03-09 22:24:59 +0200

    CORS

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, routing
 from routes import base, data
 from models import Recommender
 from contextlib import asynccontextmanager
-
+from fastapi.middleware.cors import CORSMiddleware
 
 
 @asynccontextmanager
@@ -13,6 +13,15 @@ async def lifespan(app:FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 
+
 #routers
 app.include_router(base.api_router)
 app.include_router(data.api_router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
